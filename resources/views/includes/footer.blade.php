34758e834ee15dd1333437fd7f158cffc740d6de
fatik07 <fatichur.r07@gmail.com>
2021-09-28 20:47:23 +0700

    templating

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,3 @@
 <div class="footer-left">
-  Copyright &copy; 2021 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval
-    Azhar</a>
-</div>
-<div class="footer-right">
-  2.3.0
+  Copyright &copy; 2021 <div class="bullet"></div> Design By TI 2019 A - Kelompok 9</a>
 </div>
